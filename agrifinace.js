7ef13323ac7d711613bace205a70eecc4df1c33f
Adenike Ogunmodede <adenicks727@gmail.com>
2022-01-17 22:37:05 +0100

    first commit

---
+++ agrifinace.js
@@ -0,0 +1,8 @@
+function calculateLoan(amount, rate, months){
+  let repaymentAmount = (amount*rate) + amount / months;
+  console.log(repaymentAmount)
+  //Perform a redirect to the form page using the repayment amount as a query parameter //
+  //On the plan page, retrieve the query parameter passed and display it using html  
+}
+
+/*Open an */
