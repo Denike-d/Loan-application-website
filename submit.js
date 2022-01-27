39d531b5adfb7cd46b31c5ef25f65c167fdaf534
Adenike Ogunmodede <adenicks727@gmail.com>
2022-01-27 16:16:28 +0100

    added changes to the application summary

--- submit.js
+++ submit.js
@@ -1,14 +1,22 @@
 const dataParameter = new URLSearchParams(window.location.search);
 const firstName = dataParameter.get("fname");
 const lastName = dataParameter.get("lname");
-const phoneNumber = dataParameter.get("phoneNumber")
-
+const phoneNumber = dataParameter.get("phoneNumber");
+const address = dataParameter.get("address");
 
 let heading = document.getElementById("heading");
 heading.innerHTML = "Full name : " + firstName + " " +  lastName;
 
+
+
+let farmAddress = document.getElementById("address");
+farmAddress.innerHTML = "Address : " + address;
+
 let contact = document.getElementById("contact");
-contact.innerHTML = "Phone Number : " + phoneNumber 
+contact.innerHTML = "Phone Number : " + phoneNumber;
+
+/*let address = document.getElementById("address");
+address.innerHTML = "Address : " + address;*/
 
 
 
