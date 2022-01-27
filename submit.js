1f6398e65f5fcc059f19643ba239fc4c13d0a26c
Adenike Ogunmodede <adenicks727@gmail.com>
2022-01-27 14:38:48 +0100

    created an application summary

--- submit.js
+++ submit.js
@@ -1,7 +1,14 @@
 const dataParameter = new URLSearchParams(window.location.search);
 const firstName = dataParameter.get("fname");
+const lastName = dataParameter.get("lname");
+const phoneNumber = dataParameter.get("phoneNumber")
+
 
 let heading = document.getElementById("heading");
-heading.innerHTML = "Hi " + firstName ;
+heading.innerHTML = "Full name : " + firstName + " " +  lastName;
+
+let contact = document.getElementById("contact");
+contact.innerHTML = "Phone Number : " + phoneNumber 
+
 
 
